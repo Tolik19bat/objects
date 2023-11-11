328263e717a877d53cbf6f5d407f0e71f4500325
tolik19bat <play.hoffa@gmail.com>
2023-11-11 20:08:46 +0300

    end_commit

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -5,3 +5,21 @@ test('should sum', () => {
 
   expect(result).toBe(6);
 });
+//==============================================//
+import orderByProps from "../app";
+
+test("check sort func", () => { // проверка функции сортировки
+  const obj = {
+    name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
+  };
+
+  const result = orderByProps(obj, ['name', 'level']);// результат функции
+
+  expect(result).toEqual([ // сравниваем с ожиданием
+    { key: 'name', value: 'мечник' },
+    { key: 'level', value: 2 },
+    { key: 'attack', value: 80 },
+    { key: 'defence', value: 40 },
+    { key: 'health', value: 10 },
+  ]);
+});
--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,19 @@ import sum from './basic';
 console.log('worked');
 
 console.log(sum([1, 2]));
+//=======================================================//
+export default function orderByProps(obj, arrayArgs) {
+  const arrayElements = Object.keys(obj);// список ключей объекта
+  let elements = [];                    // новый массив
+  arrayElements.forEach((key) => {      // при переборе списока
+    if (arrayArgs.includes(key)) {       // если совпадение с аргументом
+      return                            // завершаем функцию
+    } else {                            // если совпадений нет
+      elements.push(key)              // добавляем в массив
+    }
+  })
+  elements.sort();                       // сортируем массив
+  const result = [...arrayArgs, ...elements]; // соединяем в массив
+  const out = result.map((key) => ({ key, value: obj[key] })); // собираем новый массив объектов
+  return out;
+}
